c09343046703859e23c5cfc5b226ea0928c7631d
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-07-19 01:22:53 +0500

    122

--- task-one/main.py
+++ task-one/main.py
@@ -10,3 +10,13 @@ gemini_api_key = os.getenv("GEMINI_API_KEY")
 
 if not gemini_api_key:
     raise ValueError("GEMINI_API_KEY environment variable is not set.")
+
+# Set up Gemini client
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+
+#Define model
+
