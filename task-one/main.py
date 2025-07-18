32be0cdd76badfae82805d879e8e08f48e49b2e6
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-07-18 11:59:52 +0500

    11159

---
+++ task-one/main.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+import chainlit as cl
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
+from agents.run import RunConfig
+
+# Load environment variables from .env file
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY environment variable is not set.")
